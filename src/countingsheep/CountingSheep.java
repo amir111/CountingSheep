975d4c3df93bddbd56d3b10ee96f6e2810ef4172
Walt Brady <waltebrady@gmail.com>
2019-02-26 16:37:11 -0500

    Initialized DB Object
    Initialized DB Object

--- src/countingsheep/CountingSheep.java
+++ src/countingsheep/CountingSheep.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package countingsheep;
 
 import javafx.application.Application;
@@ -13,10 +9,6 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Walt
- */
 public class CountingSheep extends Application {
     
     @Override
