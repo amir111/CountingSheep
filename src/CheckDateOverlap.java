16bdaac0f23483bbfa84ef21ceaa5605d3ca4dd1
Amir Sadiev <amir.sadiev@gmail.com>
2019-04-05 16:48:00 -0400

    DB_Booking_Overlap

--- src/CheckDateOverlap.java
+++ src/CheckDateOverlap.java
@@ -10,24 +10,29 @@ import java.time.LocalDate;
 
 public class CheckDateOverlap {
 
-    public static void main(String[] args) {
+    static boolean CheckTheDatesPlox(int sYe, int sMon, int sDa, int eYe, int eMon, int eDa, String start_date22, String end_date22) {
         
         //mb try to ask user for 
         //enter start_date yyyy, now month mm, now enter dd
         //enter end_date yyye, now month me, now enter de
         // start_date2 and end_date2 could be final as they are already assigned to a room...but how? 
-        int k = 2; //it knows this will be 02
+        //int k = 2; //it knows this will be 02
+        // year, month, day 
+        Date start_date = Date.valueOf(LocalDate.of(sYe, sMon, sDa)); //can also do this to show todays date, -> Date start_date = Date.valueOf(LocalDate.now());
+        Date end_date = Date.valueOf(LocalDate.of(eYe, eMon, eDa));
         
-        Date start_date = Date.valueOf(LocalDate.of(2017, k, 10)); //can also do this to show todays date, -> Date start_date = Date.valueOf(LocalDate.now());
-        Date end_date = Date.valueOf(LocalDate.of(2019, 03, 10));
-        Date start_date2 = Date.valueOf(LocalDate.of(2019, 03, 11));
-        Date end_date2 = Date.valueOf(LocalDate.of(2019, 03, 31));
+        
+        //Date start_date2 = Date.valueOf(LocalDate.of(2019, 03, 11));
+        Date start_date2 = Date.valueOf(LocalDate.of(Integer.valueOf(start_date22.substring(0, 4)), Integer.valueOf(start_date22.substring(5, 7)), Integer.valueOf(start_date22.substring(8, 10))));
+        Date end_date2 = Date.valueOf(LocalDate.of(Integer.valueOf(end_date22.substring(0, 4)), Integer.valueOf(end_date22.substring(5, 7)), Integer.valueOf(end_date22.substring(8, 10))));
         if ((start_date.getTime() <= end_date2.getTime()) && (start_date2.getTime() <= end_date.getTime())) {
             System.out.println("conflict");
             System.out.println(start_date + " " + end_date + " " + start_date2 + " " + end_date2);
+            return false;
         } else {
             System.out.println("no conflict");
             System.out.println(start_date + " " + end_date + " " + start_date2 + " " + end_date2);
+            return true; 
         }
     }
     //Make a User Validation Check to make sure the input is only integers for dates not strings etc
--- src/DB.java
+++ src/DB.java
@@ -1,3 +1,4 @@
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -32,13 +33,13 @@ public class DB {
         }
         return uuid;
     }
-    
-    public static boolean roomExists(int userUuid, int roomNum){
+
+    public static boolean roomExists(int userUuid, int roomNum) {
         Connection conn = connect();
         Hotel hotel = DB.selectHotelByManager(userUuid);
         try {
             Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM Room WHERE hotel_id = '" + hotel.getHotelID() + "' AND number = '"+roomNum+"'");
+            ResultSet rs = statement.executeQuery("SELECT * FROM Room WHERE hotel_id = '" + hotel.getHotelID() + "' AND number = '" + roomNum + "'");
             if (rs.next()) {
                 return true;
             }
@@ -178,8 +179,8 @@ public class DB {
             while (rs.next()) {
                 RequestBooking newBooking = new RequestBooking();
                 newBooking.setRoom_id(rs.getInt("room_id"));
-                newBooking.setStart_date(rs.getDate("start_date"));
-                newBooking.setEnd_date(rs.getDate("end_date"));
+                newBooking.setStart_date(rs.getString("start_date"));
+                newBooking.setEnd_date(rs.getString("end_date"));
                 newBooking.setCustomer_id(rs.getInt("customer_id"));
                 newBooking.setBooking_id(rs.getInt("booking_id"));
                 bookings.add(newBooking);
@@ -190,15 +191,15 @@ public class DB {
         }
         return bookings;
     }
-    
+
     //NOTE: Timestamps a little buggy but it does contain all important information thats in the database
     //Returns a list of requests made by a specific client
-    public static ArrayList<PersonalRequest> selectRequestsByClient(int clientUuid){
+    public static ArrayList<PersonalRequest> selectRequestsByClient(int clientUuid) {
         ArrayList<PersonalRequest> requests = new ArrayList<>();
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM Request WHERE customer_id = "+clientUuid);
+            ResultSet rs = statement.executeQuery("SELECT * FROM Request WHERE customer_id = " + clientUuid);
             while (rs.next()) {
                 PersonalRequest newReq = new PersonalRequest();
                 newReq.setCategory(rs.getString("category"));
@@ -216,19 +217,18 @@ public class DB {
         }
         return requests;
     }
-    
+
     //Returns a list of requests based on the manager and whether the requests are complete or not
-    public static ArrayList<PersonalRequest> selectRequestsByManager(int managerUuid, boolean complete){
+    public static ArrayList<PersonalRequest> selectRequestsByManager(int managerUuid, boolean complete) {
         ArrayList<PersonalRequest> requests = new ArrayList<>();
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
             ResultSet rs;
-            if(complete){
-                rs = statement.executeQuery("SELECT * FROM Request WHERE manager_id = "+managerUuid + " AND completion_time IS NOT NULL");
-            }
-            else{
-                rs = statement.executeQuery("SELECT * FROM Request WHERE manager_id = "+managerUuid + " AND completion_time IS NULL");
+            if (complete) {
+                rs = statement.executeQuery("SELECT * FROM Request WHERE manager_id = " + managerUuid + " AND completion_time IS NOT NULL");
+            } else {
+                rs = statement.executeQuery("SELECT * FROM Request WHERE manager_id = " + managerUuid + " AND completion_time IS NULL");
             }
             while (rs.next()) {
                 PersonalRequest newReq = new PersonalRequest();
@@ -247,14 +247,14 @@ public class DB {
         }
         return requests;
     }
-    
+
     //Returns the hotel that a specific manager manages
-    public static Hotel selectHotelByManager(int managerUuid){
+    public static Hotel selectHotelByManager(int managerUuid) {
         Hotel hotel = new Hotel();
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM Hotel WHERE manager_id = '"+managerUuid+"'");
+            ResultSet rs = statement.executeQuery("SELECT * FROM Hotel WHERE manager_id = '" + managerUuid + "'");
             if (rs.next()) {
                 hotel.setHotelID(rs.getInt("hotel_id"));
                 hotel.setAddress(rs.getString("address"));
@@ -278,33 +278,21 @@ public class DB {
                     hotel.setFoodDelivery(false);
                 }
             }
-            rs = statement.executeQuery("SELECT * FROM Room WHERE hotel_id = '"+hotel.getHotelID()+"'" );
+            rs = statement.executeQuery("SELECT * FROM Room WHERE hotel_id = '" + hotel.getHotelID() + "'");
             while (rs.next()) {
                 Room newRoom = new Room();
-                newRoom.setAddress(rs.getString("address"));
-                newRoom.setCity(rs.getString("city"));
-                newRoom.setName(rs.getString("name"));
+                newRoom.setAddress(hotel.getAddress());
+                newRoom.setCity(hotel.getCity());
+                newRoom.setName(hotel.getName());
                 newRoom.setRoomDescription(rs.getString("description"));
                 newRoom.setRoomID(rs.getInt("room_id"));
                 newRoom.setRoomName(rs.getInt("number"));
                 newRoom.setRoomPrice(rs.getFloat("price"));
-                newRoom.setState(rs.getString("state"));
-                newRoom.setRating(rs.getInt("rating"));
-                if (rs.getInt("pool") == 1) {
-                    newRoom.setPool(true);
-                } else {
-                    newRoom.setPool(false);
-                }
-                if (rs.getInt("breakfast") == 1) {
-                    newRoom.setBreakfast(true);
-                } else {
-                    newRoom.setBreakfast(false);
-                }
-                if (rs.getInt("food_delivery") == 1) {
-                    newRoom.setFoodDelivery(true);
-                } else {
-                    newRoom.setFoodDelivery(false);
-                }
+                newRoom.setState(hotel.getState());
+                newRoom.setRating(hotel.getRating());
+                newRoom.setPool(hotel.isPool());
+                newRoom.setBreakfast(hotel.isBreakfast());
+                newRoom.setFoodDelivery(hotel.isFoodDelivery());
                 hotel.setRoom(newRoom);
             }
             conn.close();
@@ -331,17 +319,17 @@ public class DB {
         }
         return inserted;
     }
-    
+
     //NOTE: The Date data type will not work as java's date format is nowhere near the same as SQL's, thus maybe we should use strings or look for alternatives
     //This method creates a new booking in the database based on client input
-    public static boolean insertNewBooking(String start,String end,int customerUuid,int roomUuid) {
+    public static boolean insertNewBooking(String start, String end, int customerUuid, int roomUuid) {
         boolean inserted = false;
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
-            
+
             int uuid = getNewId("Booking", statement);
-            int numInserted = statement.executeUpdate("insert into Booking\n values('"+uuid+"','"+start+"','"+end+"','"+customerUuid+"','"+roomUuid+"');");
+            int numInserted = statement.executeUpdate("insert into Booking\n values('" + uuid + "','" + start + "','" + end + "','" + customerUuid + "','" + roomUuid + "');");
             if (numInserted > 0) {
                 inserted = true;
             }
@@ -351,33 +339,33 @@ public class DB {
         }
         return inserted;
     }
-    
+
     //This method creates a new room based on manager input
-    public static boolean insertNewRoom(int roomNumber,String description,float price,int userUuid){
+    public static boolean insertNewRoom(int roomNumber, String description, float price, int userUuid) {
         Hotel hotel = DB.selectHotelByManager(userUuid);
-        if(roomExists(userUuid,roomNumber)){
+        if (roomExists(userUuid, roomNumber)) {
             return false;
         }
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
             int uuid = getNewId("Room", statement);
-            statement.executeUpdate("insert into Room\n values('"+uuid+"','"+roomNumber+"','"+description+"','"+price+"','"+hotel.getHotelID()+"');");
+            statement.executeUpdate("insert into Room\n values('" + uuid + "','" + roomNumber + "','" + description + "','" + price + "','" + hotel.getHotelID() + "');");
             conn.close();
         } catch (Exception e) {
             throw new IllegalStateException("", e);
         }
         return true;
     }
-    
+
     //This method deletes a room within the manager's hotel
-    public static boolean deleteRoomByNumber(int roomNumber,int userUuid){
+    public static boolean deleteRoomByNumber(int roomNumber, int userUuid) {
         boolean deleted = false;
         Hotel hotel = DB.selectHotelByManager(userUuid);
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
-            int numDeleted = statement.executeUpdate("delete from Room\n where number = '"+roomNumber+"' AND hotel_id = '"+hotel.getHotelID()+"'");
+            int numDeleted = statement.executeUpdate("delete from Room\n where number = '" + roomNumber + "' AND hotel_id = '" + hotel.getHotelID() + "'");
             if (numDeleted > 0) {
                 deleted = true;
             }
@@ -387,25 +375,23 @@ public class DB {
         }
         return deleted;
     }
-    
+
     //This method updates target room number within the manager's hotel, this assume that either the description or the price is not null
-    public static boolean updateRoom(int roomNumber,int userUuid,String description,float price){
+    public static boolean updateRoom(int roomNumber, int userUuid, String description, float price) {
         Hotel hotel = DB.selectHotelByManager(userUuid);
-        if(!roomExists(userUuid,roomNumber)){
+        if (!roomExists(userUuid, roomNumber)) {
             return false;
         }
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
             String sql;
-            if(description.equals("")){
-                sql = "update Room\nset price = '"+price+"'\nwhere number = '"+roomNumber+"' and hotel_id = '"+hotel.getHotelID()+"'";
-            }
-            else if(price == 0.0f){
-                sql = "update Room\nset description = '"+description+"'\nwhere number = '"+roomNumber+"' and hotel_id = '"+hotel.getHotelID()+"'";
-            }
-            else{
-                sql = "update Room\nset description = '"+description+"', price = '"+price+"'\nwhere number = '"+roomNumber+"' and hotel_id = '"+hotel.getHotelID()+"'";
+            if (description.equals("")) {
+                sql = "update Room\nset price = '" + price + "'\nwhere number = '" + roomNumber + "' and hotel_id = '" + hotel.getHotelID() + "'";
+            } else if (price == 0.0f) {
+                sql = "update Room\nset description = '" + description + "'\nwhere number = '" + roomNumber + "' and hotel_id = '" + hotel.getHotelID() + "'";
+            } else {
+                sql = "update Room\nset description = '" + description + "', price = '" + price + "'\nwhere number = '" + roomNumber + "' and hotel_id = '" + hotel.getHotelID() + "'";
             }
             statement.executeUpdate(sql);
             conn.close();
@@ -414,14 +400,14 @@ public class DB {
         }
         return true;
     }
-    
+
     //This method sets target request as complete
-    public static boolean updateRequestAsComplete(int requestUuid){
+    public static boolean updateRequestAsComplete(int requestUuid) {
         boolean updated = false;
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
-            int numUpdated = statement.executeUpdate("update Request\nset completion_time = now()\nwhere request_id = '"+requestUuid+"'");
+            int numUpdated = statement.executeUpdate("update Request\nset completion_time = now()\nwhere request_id = '" + requestUuid + "'");
             if (numUpdated > 0) {
                 updated = true;
             }
@@ -431,14 +417,14 @@ public class DB {
         }
         return updated;
     }
-    
+
     //This method changes target user's password
-    public static boolean updateUserPassword(int UserUuid,String newPassword){
+    public static boolean updateUserPassword(int UserUuid, String newPassword) {
         boolean updated = false;
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
-            int numUpdated = statement.executeUpdate("update User\nset pword = '"+newPassword+"'\nwhere user_id = '"+UserUuid+"'");
+            int numUpdated = statement.executeUpdate("update User\nset pword = '" + newPassword + "'\nwhere user_id = '" + UserUuid + "'");
             if (numUpdated > 0) {
                 updated = true;
             }
@@ -448,35 +434,32 @@ public class DB {
         }
         return updated;
     }
-    
+
     //This method updates the manager's hotel's features
-    public static boolean updateHotelFeatures(int userUuid,boolean breakfast,boolean pool,boolean foodDelivery){
+    public static boolean updateHotelFeatures(int userUuid, boolean breakfast, boolean pool, boolean foodDelivery) {
         boolean updated = false;
         Hotel hotel = DB.selectHotelByManager(userUuid);
         Connection conn = connect();
         try {
             Statement statement = conn.createStatement();
             String sql = "update Hotel\nset";
-            if(breakfast){
-                sql += " breakfast = '"+1+"',";
+            if (breakfast) {
+                sql += " breakfast = '" + 1 + "',";
+            } else {
+                sql += " breakfast = '" + 0 + "',";
             }
-            else{
-                sql += " breakfast = '"+0+"',";
+            if (pool) {
+                sql += " pool = '" + 1 + "',";
+            } else {
+                sql += " pool = '" + 0 + "',";
             }
-            if(pool){
-                sql += " pool = '"+1+"',";
+            if (foodDelivery) {
+                sql += " food_delivery = '" + 1 + "',";
+            } else {
+                sql += " food_delivery = '" + 0 + "',";
             }
-            else{
-                sql += " pool = '"+0+"',";
-            }
-            if(foodDelivery){
-                sql += " food_delivery = '"+1+"',";
-            }
-            else{
-                sql += " food_delivery = '"+0+"',";
-            }
-            sql = sql.substring(0, sql.length()-1);
-            sql += "\nwhere hotel_id = '"+hotel.getHotelID()+"'";
+            sql = sql.substring(0, sql.length() - 1);
+            sql += "\nwhere hotel_id = '" + hotel.getHotelID() + "'";
             int numUpdated = statement.executeUpdate(sql);
             if (numUpdated > 0) {
                 updated = true;
